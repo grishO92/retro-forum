41bfeb64c95183fef6b374f791d0c4a82149b269
grishO92 <grigoryutta@gmail.com>
2022-12-11 16:20:40 +0100

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { DetailsComponent } from './components/details/details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    EditComponent,
+    DetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
